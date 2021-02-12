524a80c6633723f615d0924a4ca8546fe39ba630
DarioCozzuto <77857380+DarioCozzuto@users.noreply.github.com>
2021-02-12 20:14:15 +0100

    Add files via upload

---
+++ lettura apertura chiusura scrittura creazione file.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+</style>
+</head>
+<body>  
+<?php
+//Stamperà contenuto della pagina webdictionary.txt
+
+echo readfile("webdictionary.txt");
+
+// tutte le altre informazioni sono sui seguenti siti
+//https://www.w3schools.com/php/php_file_open.asp
+//https://www.w3schools.com/php/php_file_create.asp
+
+?>
+</body>
+</html>
---
+++ upload file.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+</style>
+</head>
+<body>  
+<?php
+//https://www.w3schools.com/php/php_file_upload.asp
+?>
+</body>
+</html>
